cc9daca1c7ba98e92fd7658196704ea7622e45e5
Joel Maupin <jmaupin82@gmail.com>
2014-03-05 21:04:34 -0600

    added database connection. Still broken but files are all uploaded

--- EventDetection/src/main/java/org/db2project/EventDetection/EventDetectorApp.java
+++ EventDetection/src/main/java/org/db2project/EventDetection/EventDetectorApp.java
@@ -1,6 +1,7 @@
 package org.db2project.EventDetection;
 
 import org.db2project.EventDetection.bl.crawler.Crawler;
+import org.db2project.EventDetection.bl.crawler.DatabaseConnection;
 
 /**
  * New Event detection using twitter streams. 
@@ -11,6 +12,8 @@ public class EventDetectorApp
     public static void main( String[] args )
     {
         System.out.println( "Event Detector started!" );
+        DatabaseConnection data = new DatabaseConnection();
+        System.exit(0);
         try{
         	Crawler.oauth(args[0], args[1], args[2], args[3]);
         } catch(Exception e){
