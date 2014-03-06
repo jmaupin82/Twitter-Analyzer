8d31fba7a158eb2931ab44383e14cd9428c4cb1c
Joel Maupin <jmaupin82@gmail.com>
2014-03-06 10:30:06 -0600

    fixed database connection

--- EventDetection/src/main/java/org/db2project/EventDetection/bl/crawler/DatabaseConnection.java
+++ EventDetection/src/main/java/org/db2project/EventDetection/bl/crawler/DatabaseConnection.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import oracle.jdbc.driver.OracleConnection;
+
 public class DatabaseConnection {
 	
 	
@@ -11,7 +13,7 @@ public class DatabaseConnection {
 	/*
 	 * Information regarding the database connection that we will use
 	 */
-	private final String DB_ADDRESS = "jdbc:oracle:thin:maup1282@//oracle.cs.ou.edu:1521/pdborcl.cs.ou.edu";
+	private final String DB_ADDRESS = "jdbc:oracle:thin:@//oracle.cs.ou.edu:1521/pdborcl.cs.ou.edu";
     private final String USER_NAME = "maup1282";
     private final String PASSWORD = "WTpy3Lp5";
     
@@ -23,7 +25,7 @@ public class DatabaseConnection {
 		
 		//open the database
 		try {
-			   this.dbConnection = DriverManager.getConnection(DB_ADDRESS,USER_NAME, PASSWORD);
+			   this.dbConnection = (OracleConnection) DriverManager.getConnection(DB_ADDRESS,USER_NAME, PASSWORD);
 			} catch (SQLException e) {
 			    e.printStackTrace();
 			    IS_OPEN = false;
